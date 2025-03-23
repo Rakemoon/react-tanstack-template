18a87273d01a8de5a23a6aeac53377d507b3e77b
Rakemoon <rakemoonkuzmin@gmail.com>
2025-03-23 13:59:18 +0700

    fix: Cirno doesnt show on deployment

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,18 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import cirno from "@/assets/cirno.png";
 
 const Route = createRootRoute({
   component: () => (
     <>
       <main className="bg-background h-screen dark text-foreground relative isolate overflow-hidden">
-        <div className="[mask-image:url('cirno.png')] [mask-repeat:no-repeat] bg-blue-300 fixed bottom-0 right-0 h-[600px] w-[300px] -z-10" />
+        <div
+          className="[mask-repeat:no-repeat] bg-blue-300 fixed bottom-0 right-0 h-[600px] w-[300px] -z-10"
+          style={{
+            maskImage: `url(${cirno})`,
+          }}
+        />
         <Outlet />
       </main>
       <TanStackRouterDevtools />
